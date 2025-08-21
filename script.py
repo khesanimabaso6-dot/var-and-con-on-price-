e99dae0b6b4a8062799e92e9d1678ad47cf04aa7
khesanimabaso6-dot <khesanimabaso6@gmail.com>
2025-08-21 09:49:52 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,6 @@
+APPLE_PRICE = 2  # constant
+BREAD_PRICE = 3  # constant
+apples = 5       # variable
+bread = 2        # variable
+total = (apples * APPLE_PRICE) + (bread * BREAD_PRICE)
+print("Total cost: $", total)
